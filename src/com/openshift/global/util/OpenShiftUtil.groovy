270a65da015ebeabc4e3fdd7a77f455327013956
Marco Yeung <myeung@redhat.com>
2018-10-30 15:13:02 -0400

    token to openshift update

--- src/com/openshift/global/util/OpenShiftUtil.groovy
+++ src/com/openshift/global/util/OpenShiftUtil.groovy
@@ -9,12 +9,11 @@ def call(String msg) {
     def templateName = 'amq7-refarch'
     def imageName = 'amq7-image'
 
-    openshift.withCluster( 'https://192.168.99.100:8443', 'fPGEwgpEqTLKwZ-ND4d4tA66niHehV7tnzotlCBcvT4' ) {
+    openshift.withCluster( 'https://192.168.99.100:8443', 'hGku3oS9N25M69yQ_Np-2_LndFAtl6XJbGL41gXfVM8' ) {
         openshift.withProject( 'fisdemo' ) {
             echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
 
-
-            openshift.newApp('https://raw.githubusercontent.com/RHsyseng/amq7/master/S2I-Base-Image/yaml_templates/amq_image_template.yaml') 
+            openshift.newApp(templatePath)
         }
     }
 }
