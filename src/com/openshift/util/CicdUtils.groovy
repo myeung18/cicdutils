336a2e10bcb5e5e00fc5029fc71be4c94d1d2a8d
Marco Yeung <myeung@redhat.com>
2018-10-30 13:16:03 -0400

    few testing library call

--- src/com/openshift/util/CicdUtils.groovy
+++ src/com/openshift/util/CicdUtils.groovy
@@ -2,7 +2,14 @@ package com.openshift.util
 
 class CicdUtils {
     static def gBuild(script, args) {
+       /* script.openshift.withCluster( 'https://192.168.99.100:8443', 'fPGEwgpEqTLKwZ-ND4d4tA66niHehV7tnzotlCBcvT4' ) {
+            script.openshift.withProject( 'fisdemo' ) {
+                echo "Hello from project ${script.openshift.project()} in cluster ${script.openshift.cluster()}"
+            }
+        } */
+
         println " ${args} " 
     }
 
 }
+return new CicdUtils()
--- vars/verify.groovy
+++ vars/verify.groovy
@@ -1,5 +1,11 @@
 def call(String msg) {
-    input "Hello --  ${msg}"
+    println "Hello --  ${msg}"
+
+    openshift.withCluster( 'https://192.168.99.100:8443', 'fPGEwgpEqTLKwZ-ND4d4tA66niHehV7tnzotlCBcvT4' ) {
+        openshift.withProject( 'fisdemo' ) {
+            echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
+        }
+    }
 }
 
 return this
