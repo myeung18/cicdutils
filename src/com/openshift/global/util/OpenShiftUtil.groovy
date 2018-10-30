4b8d04f1fa83c9f9958774fbd01fe1d42f1bc3a0
Marco Yeung <myeung@redhat.com>
2018-10-30 14:51:41 -0400

    create image with library

--- src/com/openshift/global/util/OpenShiftUtil.groovy
+++ src/com/openshift/global/util/OpenShiftUtil.groovy
@@ -3,12 +3,21 @@ package com.openshift.global.util
 
 def call(String msg) {
     println "Hello --  ${msg}"
+    def templatePath = 'https://raw.githubusercontent.com/RHsyseng/amq7/master/S2I-Base-Image/yaml_templates/amq_image_template.yaml'
+    def templateSinglePath = 'https://raw.githubusercontent.com/RHsyseng/amq7/master/S2I-Base-Image/yaml_templates/amq_single_template.yaml'
+    // name of the template that will be created
+    def templateName = 'amq7-refarch'
+    def imageName = 'amq7-image'
 
     openshift.withCluster( 'https://192.168.99.100:8443', 'fPGEwgpEqTLKwZ-ND4d4tA66niHehV7tnzotlCBcvT4' ) {
         openshift.withProject( 'fisdemo' ) {
             echo "Hello from project ${openshift.project()} in cluster ${openshift.cluster()}"
+
+
+            openshift.newApp(templatePath) 
         }
     }
 }
 
+def
 return this
