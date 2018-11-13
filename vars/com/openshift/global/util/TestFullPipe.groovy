1c1ee3f8919035ede8a9071d2bae1dca6f416d2d
Marco Yeung <myeung@redhat.com>
2018-11-13 11:50:22 -0500

    full pipe test in vars

---
+++ vars/com/openshift/global/util/TestFullPipe.groovy
@@ -0,0 +1,35 @@
+package com.openshift.global.util
+
+def call(String msg) {
+    println "Hello --  ${msg}"
+    
+    println "Environment Variables : {TIMEZONE} and ${params.TOKEN} "
+
+    sh "ls -last"
+
+    pipeline {
+        agent any
+        stages {
+            stage("fix it") {
+                steps {
+                    script {
+                        //first define openshift credential and cluster entry in Jenkins console
+                        openshift.withCluster() {
+                            openshift.withProject( 'justfortesting' ) {
+                                echo "Creating new app in project ${openshift.project()} in cluster ${openshift.cluster()}"
+                                echo "=============creating============================================"
+
+
+                                echo "=============building=======${params.TEMPLATE_PATH}====================================="
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+     
+}
+
+return this
