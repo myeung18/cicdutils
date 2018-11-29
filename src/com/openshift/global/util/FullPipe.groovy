1de018bb05ff39c9a4bd5e980ff1823e4829e891
Marco Yeung <myeung@redhat.com>
2018-11-29 16:09:27 -0500

    POC shared method

--- src/com/openshift/global/util/FullPipe.groovy
+++ src/com/openshift/global/util/FullPipe.groovy
@@ -1,25 +1,71 @@
 package com.openshift.global.util
 
 def call() {
-    //default method
-
-    echo "default method"
+    echo "***********************************************************"
+    echo " initialize the OpenShift Environment                       "
+    echo "***********************************************************"
+    /*default method
+        create new project 
+    */
 } //call
 
 def preparationWork() {
     //user pass in array of templates for resources creation
+    echo "***********************************************************"
+    echo " Setup Openshift resources                                 "
+    echo "***********************************************************"
+    openshift.withCluster( 'eng_lab' ) {
+        openshift.withProject( 'justfortesting' ) {
+            echo "Creating new app in project ${openshift.project()} in cluster ${openshift.cluster()}"
+            //openshift.create('amq62-openshift', ' --from=registry.access.redhat.com/jboss-amq-6/amq62-openshift', '--confirm') 
+            //sh 'oc login https://master.rhdp.ocp.cloud.lab.eng.bos.redhat.com:8443 --token=rgJGPpryyoPskpGpzf5e6XMytbqoXHIeLMvcZdzKCR0 --insecure-skip-tls-verify' 
+            //sh 'oc project justfortesting ; oc import-image amq62-openshift --from=registry.access.redhat.com/jboss-amq-6/amq62-openshift --confirm'
 
+        }
+    }
 }
-def checkOutAndBuild() {
+
+def checkOutSourceCode() {
+    echo "***********************************************************"
+    echo " Checkout source code from SCM                             "
+    echo "***********************************************************"
     echo "job name : ${env.JOB_NAME}"
 
-    echo "node name : ${env.NODE_NAME}"
-    echo "EXECUTOR_NUMBER name : ${env.EXECUTOR_NUMBER}"
-    echo "node name : ${env.NODE_NAME}"
-    sh 'cd fisdemoaccount;  ls -list; mvn clean'
+
+    //**** Just error handling teseting *****/
+    try {
+        timeout (time:3, unit:'SECONDS') {
+           input message:  'Is it Ok to Continue?' 
+        }
+    } catch (exception) {
+        echo "I didn't get user input"
+    }
+    git 'https://github.com/myeung18/fuse-financial-cicd.git'
+
 }
 
-def checkout() {
+def buildAndTest() {
+    echo "***********************************************************"
+    echo " Build with SCM                                            "
+    echo "***********************************************************"
+    echo "node name : ${env.NODE_NAME}"
+    sh 'cd fisdemoaccount; ls -list; mvn clean'
 }
 
+def deploy() {
+    echo "***********************************************************"
+    echo " Deploy to OpenShift                                       "
+    echo "***********************************************************"
+
+    sh 'cd fisdemoaccount; ls -list;'  
+    sh 'mvn fabric8:deploy -Dmysql-service-username=dbuser -Dmysql-service-password=password'
+}
+
+def moveToUAT() {
+
+}
+
+def moveToProd() {
+    
+}
 return this
--- src/com/openshift/util/CicdUtils.groovy
+++ src/com/openshift/util/CicdUtils.groovy
@@ -1,6 +1,35 @@
 package com.openshift.util
 
 class CicdUtils {
+    static def cmdDeploy(args) {
+        sh ''' 
+            
+            cd ${args}
+
+            mvn package -Dmaven.test.skip=true 
+            
+            oc login https://master.rhdp.ocp.cloud.lab.eng.bos.redhat.com:8443 --token=WddnuYe5y7_7CslKND9tWdS2vn6CRLR5eRu5OlOrITI 
+            
+            oc project justfortesting                    
+
+            mvn fabric8:deploy -Dmaven.test.skip=true -Dmysql-service-username=root -Dmysql-service-password=ncPIGN8cKa5Aki4c   
+        '''
+    }
+    
+    static def cmdNpmDeploy(args) {
+        sh '''
+            cd ${args}
+
+            oc login https://master.rhdp.ocp.cloud.lab.eng.bos.redhat.com:8443 --token=WddnuYe5y7_7CslKND9tWdS2vn6CRLR5eRu5OlOrITI 
+            
+            oc project justfortesting                    
+
+            export PATH=$PATH:/usr/local/bin 
+                
+            npm install && npm run openshift
+        '''
+    }
+    
     static def gBuild(script, args) {
        /* script.openshift.withCluster( 'https://192.168.99.100:8443', 'fPGEwgpEqTLKwZ-ND4d4tA66niHehV7tnzotlCBcvT4' ) {
             script.openshift.withProject( 'fisdemo' ) {
