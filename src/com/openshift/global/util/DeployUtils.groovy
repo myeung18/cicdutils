bf76d5c58de0666810d33513be1ffed074be1c47
Marco Yeung <myeung@redhat.com>
2018-11-29 16:23:12 -0500

    method in procedure

---
+++ src/com/openshift/global/util/DeployUtils.groovy
@@ -0,0 +1,19 @@
+package com.openshift.global.util
+
+def cmdDeploy(args) {
+     println "passed in ${args} " 
+    sh ''' 
+        
+        cd fisuser-service  
+
+        mvn package -Dmaven.test.skip=true 
+        
+        oc login https://master.rhdp.ocp.cloud.lab.eng.bos.redhat.com:8443 --token=WddnuYe5y7_7CslKND9tWdS2vn6CRLR5eRu5OlOrITI 
+        
+        oc project justfortesting                    
+
+        mvn fabric8:deploy -Dmaven.test.skip=true -Dmysql-service-username=root -Dmysql-service-password=ncPIGN8cKa5Aki4c   
+    '''
+}
+
+
