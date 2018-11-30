24912baa4eb4b74c634543b2c66aaf317566084a
Marco Yeung <myeung@redhat.com>
2018-11-30 13:58:13 -0500

    Jennkins node test

--- src/com/openshift/global/util/DeployUtils.groovy
+++ src/com/openshift/global/util/DeployUtils.groovy
@@ -2,7 +2,6 @@ package com.openshift.global.util
 
 def cmdDeploy() {
     println "passed in  ${env.serviceName} " 
-    node('maven') {
     sh ''' 
         
         cd ${serviceName}
@@ -12,8 +11,7 @@ def cmdDeploy() {
         oc project justfortesting                    
 
         mvn fabric8:deploy -Dmaven.test.skip=true -Dmysql-service-username=root -Dmysql-service-password=ncPIGN8cKa5Aki4c   
-        '''
-    }
+    '''
 }
 
 def cmdNpmDeploy() {
